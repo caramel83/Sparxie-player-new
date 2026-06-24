4b4b2caa5a2871a4531bb20b3738da04ffd430d0
caramel83 <xdfox2030@gmail.com>
2026-06-24 05:56:27 +0300

    Update gameManager.js

--- gameManager.js
+++ gameManager.js
@@ -1,5 +1,10 @@
 // مدير حالة الألعاب النشطة بكل قناة
+// يدعم نوعين من الحالة بنفس الوقت:
+//   1) activeGames      -> اللعبة المفتوحة بالقناة (تلقائية أو يدوية بدون خصم محدد)
+//   2) activeChallenges -> تحدي 1v1 (أو جماعي مفتوح المشاركة) لكل قناة
+
 const activeGames = new Map();
+const activeChallenges = new Map(); // key: channelId -> challenge object
 
 function setActiveGame(channelId, gameData) {
   activeGames.set(channelId, gameData);
@@ -13,4 +18,23 @@ function clearActiveGame(channelId) {
   activeGames.delete(channelId);
 }
 
-module.exports = { setActiveGame, getActiveGame, clearActiveGame };
+function setActiveChallenge(channelId, challengeData) {
+  activeChallenges.set(channelId, challengeData);
+}
+
+function getActiveChallenge(channelId) {
+  return activeChallenges.get(channelId);
+}
+
+function clearActiveChallenge(channelId) {
+  activeChallenges.delete(channelId);
+}
+
+module.exports = {
+  setActiveGame,
+  getActiveGame,
+  clearActiveGame,
+  setActiveChallenge,
+  getActiveChallenge,
+  clearActiveChallenge,
+};
