6bbfbdd5a3b24511aee1cbfd6759a13709c664c2
caramel83 <xdfox2030@gmail.com>
2026-07-26 10:51:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 require("dotenv").config();
+
+// ===== Keep-alive server (يمنع Render من تنويم البوت) =====
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Sparxie is alive!"));
+app.listen(process.env.PORT || 3000, () => console.log("🌐 Keep-alive server running"));
+
 const {
   Client,
   GatewayIntentBits,
